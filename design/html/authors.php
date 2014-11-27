df988e3c48ad28e6da520da9f7606aee185fca16
Arjuna <arjun.kashyap@yahoo.co.in>
2014-11-27 21:11:40 +0530

    Commit No. 67 - Author listing completed

--- design/html/authors.php
+++ design/html/authors.php
@@ -1,36 +1,37 @@
 <?php include("include_header.php");?>
-<?php include("include_nav.php");?>
-		<div class="archive_holder">
-			<div class="page_title"><i class="fa fa-user"></i>&nbsp;&nbsp;Authors</div>
-			<div class="alphabet">
-				<span class="letter"><a href="authors.php?letter=A">A</a></span>
-				<span class="letter"><a href="authors.php?letter=B">B</a></span>
-				<span class="letter"><a href="authors.php?letter=C">C</a></span>
-				<span class="letter"><a href="authors.php?letter=D">D</a></span>
-				<span class="letter"><a href="authors.php?letter=E">E</a></span>
-				<span class="letter"><a href="authors.php?letter=F">F</a></span>
-				<span class="letter"><a href="authors.php?letter=G">G</a></span>
-				<span class="letter"><a href="authors.php?letter=H">H</a></span>
-				<span class="letter"><a href="authors.php?letter=I">I</a></span>
-				<span class="letter"><a href="authors.php?letter=J">J</a></span>
-				<span class="letter"><a href="authors.php?letter=K">K</a></span>
-				<span class="letter"><a href="authors.php?letter=L">L</a></span>
-				<span class="letter"><a href="authors.php?letter=M">M</a></span>
-				<span class="letter"><a href="authors.php?letter=N">N</a></span>
-				<span class="letter"><a href="authors.php?letter=O">O</a></span>
-				<span class="letter"><a href="authors.php?letter=P">P</a></span>
-				<span class="letter"><a href="authors.php?letter=Q">Q</a></span>
-				<span class="letter"><a href="authors.php?letter=R">R</a></span>
-				<span class="letter"><a href="authors.php?letter=S">S</a></span>
-				<span class="letter"><a href="authors.php?letter=T">T</a></span>
-				<span class="letter"><a href="authors.php?letter=U">U</a></span>
-				<span class="letter"><a href="authors.php?letter=V">V</a></span>
-				<span class="letter"><a href="authors.php?letter=W">W</a></span>
-				<span class="letter"><a href="authors.php?letter=X">X</a></span>
-				<span class="letter"><a href="authors.php?letter=Y">Y</a></span>
-				<span class="letter"><a href="authors.php?letter=Z">Z</a></span>
-			</div>
-				<ul class="dot">
+<main class="cd-main-content">
+		<div class="cd-scrolling-bg cd-color-2">
+			<div class="cd-container">
+				<h1 class="clr1">Archive &gt; Authors</h1>
+				<div class="alphabet gapBelowSmall gapAboveSmall">
+					<span class="letter"><a href="authors.php?letter=A">A</a></span>
+					<span class="letter"><a href="authors.php?letter=B">B</a></span>
+					<span class="letter"><a href="authors.php?letter=C">C</a></span>
+					<span class="letter"><a href="authors.php?letter=D">D</a></span>
+					<span class="letter"><a href="authors.php?letter=E">E</a></span>
+					<span class="letter"><a href="authors.php?letter=F">F</a></span>
+					<span class="letter"><a href="authors.php?letter=G">G</a></span>
+					<span class="letter"><a href="authors.php?letter=H">H</a></span>
+					<span class="letter"><a href="authors.php?letter=I">I</a></span>
+					<span class="letter"><a href="authors.php?letter=J">J</a></span>
+					<span class="letter"><a href="authors.php?letter=K">K</a></span>
+					<span class="letter"><a href="authors.php?letter=L">L</a></span>
+					<span class="letter"><a href="authors.php?letter=M">M</a></span>
+					<span class="letter"><a href="authors.php?letter=N">N</a></span>
+					<span class="letter"><a href="authors.php?letter=O">O</a></span>
+					<span class="letter"><a href="authors.php?letter=P">P</a></span>
+					<span class="letter"><a href="authors.php?letter=Q">Q</a></span>
+					<span class="letter"><a href="authors.php?letter=R">R</a></span>
+					<span class="letter"><a href="authors.php?letter=S">S</a></span>
+					<span class="letter"><a href="authors.php?letter=T">T</a></span>
+					<span class="letter"><a href="authors.php?letter=U">U</a></span>
+					<span class="letter"><a href="authors.php?letter=V">V</a></span>
+					<span class="letter"><a href="authors.php?letter=W">W</a></span>
+					<span class="letter"><a href="authors.php?letter=X">X</a></span>
+					<span class="letter"><a href="authors.php?letter=Y">Y</a></span>
+					<span class="letter"><a href="authors.php?letter=Z">Z</a></span>
+					<span class="letter"><a href="authors.php?letter=Special">#</a></span>
+				</div>
 <?php
 
 include("connect.php");
@@ -39,71 +40,49 @@ require_once("common.php");
 if(isset($_GET['letter']))
 {
 	$letter=$_GET['letter'];
-
+	
 	if(!(isValidLetter($letter)))
 	{
-		echo "<li>Invalid URL</li>";
-		
-		echo "</ul>";
-        echo "</div></div>";
-        include("include_footer.php");
+		echo '<span class="aFeature clr2">Invalid URL</span>';
+		echo '</div> <!-- cd-container -->';
+		echo '</div> <!-- cd-scrolling-bg -->';
+		echo '</main> <!-- cd-main-content -->';
+		include("include_footer.php");
+
         exit(1);
 	}
-
-	if($letter == '')
-	{
-		$letter = 'A';
-	}
+	
+	($letter == '') ? $letter = 'A' : $letter = $letter;
 }
 else
 {
 	$letter = 'A';
 }
 
-$db = @new mysqli('localhost', "$user", "$password", "$database");
-if($db->connect_errno > 0)
-{
-	echo '<li>Not connected to the database [' . $db->connect_errno . ']</li>';
-	echo "</ul>";
-    echo "</div></div>";
-    include("include_footer.php");
-	exit(1);
-}
-
-
-$query = "select * from author where authorname like '$letter%' order by authorname";
-/*
-$query = "select * from author where authorname like '$letter%' order by authorname";
-*/
-
+$query = 'select * from author where authorname like \'' . $letter . '%\' order by authorname';
 
 $result = $db->query($query); 
 $num_rows = $result ? $result->num_rows : 0;
 
 if($num_rows > 0)
 {
-	for($i=1;$i<=$num_rows;$i++)
+	while($row = $result->fetch_assoc())
 	{
-				$row = $result->fetch_assoc();
-
-		$authid=$row['authid'];
-		$authorname=$row['authorname'];
-
-		echo "<li>";
-		echo "<span class=\"authorspan\"><a href=\"auth.php?authid=$authid&amp;author=" . urlencode($authorname) . "\">$authorname</a></span>";
-		echo "</li>\n";
+		echo '<div class="author">';
+		echo '	<span class="aAuthor"><a href="auth.php?authid=' . $row['authid'] . '&amp;author=' . urlencode($row['authorname']) . '">' . $row['authorname'] . '</a> ';
+		echo '</div>';
 	}
 }
 else
 {
-	echo "<li>Sorry! No author names were found to begin with the letter '$letter' in The Vedanta Kesari</li>";
+	echo "Sorry! No author names were found to begin with the letter '$letter' in The Vedanta Kesari";
 }
 
 if($result){$result->free();}
 $db->close();
+
 ?>
-				</ul>
-			</div>
-	</div>
-    
+			</div> <!-- cd-container -->
+		</div> <!-- cd-scrolling-bg -->
+	</main> <!-- cd-main-content -->
 <?php include("include_footer.php");?>
