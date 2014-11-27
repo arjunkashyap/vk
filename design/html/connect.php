efe508180ded90af7772e6f891aa157e63760581
Arjuna <arjun.kashyap@yahoo.co.in>
2014-11-27 21:00:09 +0530

    Commit No. 66 - Database connection validation pushed to connect.php

--- design/html/connect.php
+++ design/html/connect.php
@@ -7,11 +7,13 @@ $type_code = '01';
 $db = @new mysqli('localhost', "$user", "$password", "$database");
 if($db->connect_errno > 0)
 {
-	echo '<li>Not connected to the database [' . $db->connect_errno . ']</li>';
-	echo "</ul>";
-    echo "</div></div>";
-    include("include_footer.php");
-	exit(1);
+	echo '<span class="aFeature clr2">Not connected to the Database</span>';
+	echo '</div> <!-- cd-container -->';
+	echo '</div> <!-- cd-scrolling-bg -->';
+	echo '</main> <!-- cd-main-content -->';
+	include("include_footer.php");
+
+    exit(1);
 }
 
 $month_name = array("0"=>"","1"=>"January","2"=>"February","3"=>"March","4"=>"April","5"=>"May","6"=>"June","7"=>"July","8"=>"August","9"=>"September","10"=>"October","11"=>"November","12"=>"December");
